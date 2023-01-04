c06cd9489d79f1601c7506b930b0c900cfb5515f
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2023-01-03 23:48:26 -0800

    Added Framer and finished header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -7,7 +8,22 @@ export default function Header({}: Props) {
 	return (
 		<header className="flex-items-start sticky top-0 z-20 mx-auto flex max-w-7xl justify-between p-5 xl:items-center">
 			{/* Icons */}
-			<div className="flex flex-row items-center">
+			<motion.div
+				initial={{
+					x: -500,
+					opacity: 0,
+					scale: 0,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 1.5,
+				}}
+				className="flex flex-row items-center"
+			>
 				<SocialIcon
 					url="https://github.com/sgarg15"
 					fgColor="gray"
@@ -23,9 +39,24 @@ export default function Header({}: Props) {
 					fgColor="gray"
 					bgColor="transparent"
 				/>
-			</div>
+			</motion.div>
 
-			<div className="flex cursor-pointer flex-row items-center text-gray-300">
+			<motion.div
+				initial={{
+					x: 500,
+					opacity: 0,
+					scale: 0,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 1.5,
+				}}
+				className="flex cursor-pointer flex-row items-center text-gray-300"
+			>
 				<SocialIcon
 					className="cursor-pointer"
 					network="email"
@@ -36,7 +67,7 @@ export default function Header({}: Props) {
 				<p className="hidden text-sm uppercase text-gray-400 md:inline-flex">
 					Get in Touch
 				</p>
-			</div>
+			</motion.div>
 		</header>
 	);
 }
