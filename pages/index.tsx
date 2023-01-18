8fdd53346a5880f71bb6a16756b98b6a89c872c4
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2023-01-17 20:05:35 -0800

    Added Scrollbar design

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ repoCount }: any) {
 	return (
-		<div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden scroll-smooth bg-[rgb(36,36,36)] text-white">
+		<div className="z-0 h-screen snap-y snap-mandatory scroll-smooth bg-[rgb(36,36,36)] text-white overflow-x-hidden scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/40 scrollbar-thumb-rounded">
 			<Head>
 				<title>Create Next App</title>
 			</Head>
