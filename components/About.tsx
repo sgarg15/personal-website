b32bea180bca7c8e835370e3a99604ea5fac6ef0
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2023-01-23 12:05:42 -0800

    Fixed About Page sizing issues

--- components/About.tsx
+++ components/About.tsx
@@ -21,64 +21,66 @@ export default function About({ repoCount }: any) {
 			transition={{
 				duration: 1.5,
 			}}
-			className="relative mx-auto flex h-screen max-w-7xl flex-col items-center justify-evenly px-10 text-center md:flex-row md:text-left "
+			className="mx-auto flex h-screen max-w-7xl flex-col items-center justify-evenly px-10 text-center	md:text-left"
 		>
-			<h3 className="absolute top-24 select-none pl-[20px] text-center text-2xl uppercase tracking-[20px] text-gray-500">
+			<h3 className=" select-none pl-[20px] text-center text-2xl uppercase tracking-[20px] text-gray-500 max-md:mx-auto max-md:mt-4 max-md:text-center">
 				About
 			</h3>
 
-			<motion.img
-				src="/Satvik.jpg"
-				initial={{
-					x: -200,
-					opacity: 0,
-				}}
-				transition={{
-					duration: 1.2,
-				}}
-				whileInView={{
-					x: 0,
-					opacity: 1,
-				}}
-				viewport={{ once: true }}
-				className="md:h-95 -mb-20 h-44 w-44 flex-shrink-0 items-center rounded-full object-cover text-center md:mb-0 md:w-64 md:rounded-lg xl:h-[400px] xl:w-[400px]"
-			/>
+			<div className="flex justify-center align-middle max-md:flex-col max-sm:flex">
+				<motion.img
+					src="/Satvik.jpg"
+					initial={{
+						x: -200,
+						opacity: 0,
+					}}
+					transition={{
+						duration: 1.2,
+					}}
+					whileInView={{
+						x: 0,
+						opacity: 1,
+					}}
+					viewport={{ once: true }}
+					className="-mb-20 h-44 w-44 flex-shrink-0 items-center rounded-full object-cover text-center max-md:my-4 max-md:mx-auto max-md:h-[200px] max-md:w-[200px] max-md:items-center md:mb-0 md:h-[300px] md:w-[300px] md:rounded-lg xl:h-[400px] xl:w-[400px]"
+				/>
 
-			<div className="space-y-10 px-0 md:px-10">
-				<h4 className="text-4xl font-semibold">
-					Here is a{" "}
-					<span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
-					background
-				</h4>
-				<p className="text-base">
-					Hi, I'm Satvik. I'm a second year student in Simon Fraser University
-					majoring in Computer Science. I have always been passionate about
-					programming and technologies in general. So I try to channel that
-					passion through projects such as this website as well as many others
-					found on my resume.
-				</p>
+				<div className="space-y-10 px-0 align-middle sm:my-4 sm:w-full md:px-10">
+					<h4 className="text-4xl font-semibold">
+						Here is a{" "}
+						<span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+						background
+					</h4>
+					<p className="text-base">
+						Hi, I'm Satvik. I'm a second year student in Simon Fraser University
+						majoring in Computer Science. I have always been passionate about
+						programming and technologies in general. So I try to channel that
+						passion through projects such as this website as well as many others
+						found on my resume.
+					</p>
 
-				<div className="ml-10 flex flex-col ">
-					<div className="flex items-center">
-						<BsGithub className="mr-5 inline-block text-3xl" />
-						<p className="text-left text-base uppercase tracking-widest text-gray-300">
-							{repoCount} Repositories
-						</p>
-					</div>
-					<div className="mt-5 flex items-center">
-						<BsCode className="mr-5 inline-block text-3xl" />
-						<p className="text-left text-base uppercase tracking-widest text-gray-300 ">
-							{date.getFullYear() - 2017} Years of Programming
-						</p>
-					</div>
-					<div className="mt-5 flex cursor-pointer items-center text-left">
-						<AiOutlinePaperClip className="mr-5 inline-block text-3xl" />
-						<p
-							onClick={handleClick}
-							className="text-base uppercase tracking-widest text-gray-300 "
-						>
-							Download My Resume
-						</p>
+					<div className="ml-10 flex flex-col ">
+						<div className="flex items-center">
+							<BsGithub className="mr-5 inline-block text-3xl" />
+							<p className="text-left text-base uppercase tracking-widest text-gray-300">
+								{repoCount} Repositories
+							</p>
+						</div>
+						<div className="mt-5 flex items-center">
+							<BsCode className="mr-5 inline-block text-3xl" />
+							<p className="text-left text-base uppercase tracking-widest text-gray-300 ">
+								{date.getFullYear() - 2017} Years of Programming
+							</p>
+						</div>
+						<div className="mt-5 flex cursor-pointer items-center text-left">
+							<AiOutlinePaperClip className="mr-5 inline-block text-3xl" />
+							<p
+								onClick={handleClick}
+								className="text-base uppercase tracking-widest text-gray-300 "
+							>
+								Download My Resume
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
