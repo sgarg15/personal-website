66ca16cfb7d9a01cc0fb8d76ec017bfc648f49f0
Satvik Garg <47345135+sgarg15@users.noreply.github.com>
2023-02-22 16:17:14 -0800

    Changed site name and text unselectable mobile

--- components/MobilePlaceHolder.tsx
+++ components/MobilePlaceHolder.tsx
@@ -9,12 +9,12 @@ export const MobilePlaceHolder = () => {
 			animate={{ opacity: 1 }}
 			transition={{ duration: 0.5 }}
 		>
-			<div className="rounded-lg p-6 shadow-lg">
-				<h2 className="mb-4 text-2xl font-bold text-gray-300">
+			<div className="rounded-lg p-6 shadow-lg ">
+				<h2 className="mb-4 select-none text-2xl font-bold text-gray-300">
 					This site is currently unavailable in mobile.
 				</h2>
-				<p className="mb-6 text-lg text-gray-500">
-					Please open sgarg.vercel.com on a desktop to get the full experience!
+				<p className="mb-6 select-none text-lg text-gray-500">
+					Please open sgarg.vercel.app on a desktop to get the full experience!
 				</p>
 			</div>
 		</motion.div>
